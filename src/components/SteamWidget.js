be6a84a364b04060732c75249d9ed58e3c521f0a
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-09-20 01:14:08 -0600

    fixed alt text

--- src/components/SteamWidget.js
+++ src/components/SteamWidget.js
@@ -10,7 +10,7 @@ const SteamWidget = () => {
     </div>
     <div className="desc">
         <a href="https://store.steampowered.com/app/2100970/Curl/" target="_blank" rel="noreferrer">
-            <img src={capsule} alt="small logo image" className="logo" />
+            <img src={capsule} alt="small logo" className="logo" />
         </a>
         <p>
             The beloved and captivating sport is spreading to new worlds! Take up the broom to challenge your friends locally or online to a competition of wit, teamwork, and silliness in this first-of-its-kind game, built for accessibility with brain-computer interface integration.
