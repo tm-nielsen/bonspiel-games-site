4d52602d06e7ef4b2749cefc914c52663e414af1
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-10-25 13:54:32 -0600

    added mail link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { FaSteam, FaTwitter, FaDiscord } from 'react-icons/fa'
+import { FaSteam, FaTwitter, FaDiscord, FaEnvelope } from 'react-icons/fa'
 import '../styling/footer.css'
 
 const Footer = () => {
   return <div className="footer">
     <a href="https://store.steampowered.com/app/2100970/Curl/" target="_blank" rel='noreferrer' className="footer-icon-box">
-        <FaSteam className='footer-icon'/>
+      <FaSteam className='footer-icon'/>
+    </a>
+    <a href="mailto: braincurling@gmail.com" target="_blank" rel='noreferrer' classname="footer-icon-box">
+      <FaEnvelope className="footer-icon"/>
     </a>
     <a href="https://twitter.com/bonspiel_games" target="_blank" rel='noreferrer' className="footer-icon-box">
-        <FaTwitter className='footer-icon'/>
+      <FaTwitter className='footer-icon'/>
     </a>
     <a href="https://discord.gg/FBn84MpCfS" target="_blank" rel='noreferrer' className="footer-icon-box">
-        <FaDiscord className='footer-icon'/>
+      <FaDiscord className='footer-icon'/>
     </a>
   </div>
 }
